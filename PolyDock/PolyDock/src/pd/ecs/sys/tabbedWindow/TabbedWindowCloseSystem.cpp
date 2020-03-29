7a4225d1a1d38a897e937583fbe07037b9554b72
Squares <m.piotr@moscicki.pl>
2020-03-29 10:45:53 +0200

    Add some files WIP

---
+++ PolyDock/PolyDock/src/pd/ecs/sys/tabbedWindow/TabbedWindowCloseSystem.cpp
@@ -0,0 +1,53 @@
+#include <pd/pch/PCH.h>
+#include <pd/ecs/sys/tabbedWindow/TabbedWindowWidgetUpdateSystem.hpp>
+
+// in
+#include <pd/ecs/cmp/tabbedWindow/DirtyTabbedWindowComponent.hpp>
+#include <pd/ecs/cmp/tabbedWindow/TabbedWindowComponent.hpp>
+#include <pd/ecs/cmp/tabsHeader/TabsHeaderWidgetComponent.hpp>
+#include <pd/ecs/cmp/tabsHeader/ActiveTabComponent.hpp>
+#include <pd/ecs/cmp/tabbedWindowControl/WidgetComponent.hpp>
+#include <pd/ecs/cmp/tab/TabContentComponent.hpp>
+// out
+#include <pd/ecs/cmp/tabbedWindow/TabbedWindowWidgetComponent.hpp>
+
+using namespace ::pd::ecs::sys::tabbedWindow;
+using namespace ::pd::ecs::cmp::tabbedWindow;
+using namespace ::pd::ecs::cmp::tabsHeader;
+using namespace ::pd::ecs::cmp;
+using namespace ::pd::ecs::cmp::tab;
+
+// ---------------------------------------------------------------------------------------------------------
+void TabbedWindowWidgetUpdateSystem::update(entt::registry& registry, entt::entity root) const
+{
+	auto view = registry.view<
+		DirtyTabbedWindowComponent,
+		TabbedWindowComponent,
+		TabbedWindowWidgetComponent,
+		tabbedWindowControl::WidgetComponent,
+		TabsHeaderWidgetComponent>();
+
+	for (auto entity : view)
+	{
+		auto& window = view.get<TabbedWindowComponent>(entity);
+		auto& widget = view.get<TabbedWindowWidgetComponent>(entity);
+		auto& tabsHeaderWidget = view.get<TabsHeaderWidgetComponent>(entity);
+		auto& controlWidget = view.get<tabbedWindowControl::WidgetComponent>(entity);
+
+		widget.setPos(window.position);
+		widget.setSize(window.size);
+		widget.setTabsHeaderWidget(tabsHeaderWidget.getTabsHeaderWidget());
+
+		if (auto* activeTabCmp = registry.try_get<ActiveTabComponent>(entity))
+		{
+			if (auto* tabContentCmp = registry.try_get<TabContentComponent>(activeTabCmp->activeTab))
+				widget.setContentWidget(tabContentCmp->contentWidget);
+			else
+				widget.setContentWidget(nullptr);
+		}
+		else
+			widget.setContentWidget(nullptr);
+
+		registry.remove<DirtyTabbedWindowComponent>(entity);
+	}
+}
