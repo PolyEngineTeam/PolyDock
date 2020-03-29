3efdf2c38f07a79560df8138c6809de6462aff79
Squares <m.piotr@moscicki.pl>
2020-03-29 10:39:13 +0200

    Add TabbedWindowControlHandle

---
+++ PolyDock/PolyDock/src/pd/TabbedWindowControlHandle.cpp
@@ -0,0 +1,50 @@
+#include <pd/pch/PCH.h>
+#include <pd/TabbedWindowControlHandle.hpp>
+
+#include <pd/ecs/cmp/tabbedWindowControl/ObserversComponent.hpp>
+#include <pd/ecs/cmp/tabbedWindowControl/CloseRequestComponent.hpp>
+
+using namespace ::pd;
+using namespace ::pd::ecs::cmp::tabbedWindowControl;
+
+// ---------------------------------------------------------------------------------------------------------
+void TabbedWindowControlHandle::registerObserver(Observer* observer)
+{
+	m_registry.get_or_assign<ObserversComponent>(m_entity).observers.push_back(observer);
+}
+
+// ---------------------------------------------------------------------------------------------------------
+void TabbedWindowControlHandle::unregisterObserver(Observer* observer)
+{
+	std::vector<TabbedWindowControlHandle::Observer*>& observers = 
+		m_registry.get_or_assign<ObserversComponent>(m_entity).observers;
+		
+	observers.erase(std::remove(observers.begin(), observers.end(), observer));
+}
+
+// ---------------------------------------------------------------------------------------------------------
+bool TabbedWindowControlHandle::canClose()
+{
+	std::vector<TabbedWindowControlHandle::Observer*>& observers =
+		m_registry.get_or_assign<ObserversComponent>(m_entity).observers;
+
+	for (Observer* obs : observers)
+	{
+		if (!obs->canClose())
+			return false;
+	}
+
+	return true;
+}
+
+// ---------------------------------------------------------------------------------------------------------
+void TabbedWindowControlHandle::tryClose()
+{
+	m_registry.get_or_assign<CloseRequestComponent>(m_entity);
+}
+
+// ---------------------------------------------------------------------------------------------------------
+void TabbedWindowControlHandle::forceClose()
+{
+	m_registry.get_or_assign<CloseRequestComponent>(m_entity, true);
+}
